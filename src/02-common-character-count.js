b24f5b41d31d6d2ea614ab7253e6a71941521b68
Kseniya Barkova <kcancer17@gmail.com>
2021-04-09 21:23:51 +0300

    feat: add the solution

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,16 +1,14 @@
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  const arr2 = str2.split("");
+  for(char of str1) {
+    let indexArr2 = arr2.findIndex(s => s === char);
+    if(indexArr2 >= 0) {
+      count++;
+      console.log(arr2.splice(indexArr2, 1));
+    }
+  }
+  return count;
 }
 
 module.exports = getCommonCharacterCount;
