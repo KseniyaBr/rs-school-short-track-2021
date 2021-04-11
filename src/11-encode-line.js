4836541defbcbc93c103623daa7a9e1e97817780
Kseniya Barkova <kcancer17@gmail.com>
2021-04-11 13:12:39 +0300

    feat: add the solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,19 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
-}
+function encodeLine(str) {
+
+  let count = 1;
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    }
+    if (str[i] !== str[i + 1]) {
+      newStr = newStr.concat(`${count === 1 ? '' : count}${str[i]}`);
+      count = 1;
+    }
+  }
+  return newStr;
 
 module.exports = encodeLine;
