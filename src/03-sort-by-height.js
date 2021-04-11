0f8300f50177da1a78d04cf0dd09ffd04211beec
Kseniya Barkova <kcancer17@gmail.com>
2021-04-11 21:25:35 +0300

    fix: fix the solution

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -10,9 +10,10 @@
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 function sortByHeight(arr) {
-  let x = 0;
+  const x = 0;
   const result = arr;
-  const sortedArr = arr.filter((i) => i !== -1).sort((a, b) => a - b);
+  const sortedArr = arr.filter((i) => i !== -1).sort(function(a, b) {
+    return a - b});
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== -1) {
       result[i] = sortedArr[x];
